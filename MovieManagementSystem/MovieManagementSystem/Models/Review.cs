4083a1c0b77dcbda5c001b1cde95fa13c5694c09
Chkheto <chkhetianisandro808@gmail.com>
2026-05-19 19:58:21 +0400

    Add files via upload

---
+++ MovieManagementSystem/MovieManagementSystem/Models/Review.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieManagementSystem.Models
+{
+    internal class Review
+    {
+      
+        public string UserEmail { get; set; } = string.Empty;
+        public double Rating { get; set; }
+        public string Comment { get; set; } = string.Empty;
+        public DateTime DatePosted { get; set; }
+    }
+}
